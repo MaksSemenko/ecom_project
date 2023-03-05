d876a5604e080f83592d2c0d72c066f9d4a32815
Maks Semenko <max_semenko@ukr.net>
2023-03-05 17:30:26 +0200

    fix bug with cookies

--- store/utils.py
+++ store/utils.py
@@ -7,33 +7,34 @@ def cookieCart(request):
         cart = json.loads(request.COOKIES['cart'])
     except:
         cart = {}
-
-    print('Cart', cart)
+        print('Cart', cart)
     items = []
     order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
     cartItems = order['get_cart_items']
 
     for i in cart:
         try:
-            cartItems += cart[i]['quantity']
-            product = Product.objects.get(id=i)
-            total = product.price * cart[i]['quantity']
-            order['get_cart_total'] += total
-            order['get_cart_items'] += cart[i]['quantity']
+            if cart[i]['quantity']:
+                cartItems += cart[i]['quantity']
+                product = Product.objects.get(id=i)
+                total = product.price * cart[i]['quantity']
+                order['get_cart_total'] += total
+                order['get_cart_items'] += cart[i]['quantity']
 
-            item = {
-                'product': {
+                item = {
                     'id': product.id,
-                    'name': product.name,
-                    'price': product.price,
-                    'imageURL': product.imageURL,
-                },
-                'quantity': cart[i]['quantity'],
-                'get_total': total,
-            }
-            items.append(item)
-            if product.digital is False:
-                order['shipping'] = True
+                    'product': {
+                        'id': product.id,
+                        'name': product.name,
+                        'price': product.price,
+                        'imageURL': product.imageURL,
+                    },
+                    'quantity': cart[i]['quantity'],
+                    'get_total': total,
+                }
+                items.append(item)
+                if product.digital is False:
+                    order['shipping'] = True
         except:
             pass
     return {'cartItems': cartItems, 'order': order, 'items': items}
@@ -54,8 +55,6 @@ def cartData(request):
 
 
 def guestOrder(request, data):
-    print('User is not logged in..')
-    print('COOKIES:', request.COOKIES)
     name = data['form']['name']
     email = data['form']['email']
     cookieData = cookieCart(request)
@@ -66,6 +65,6 @@ def guestOrder(request, data):
 
     order = Order.objects.create(customer=customer, complete=False)
     for item in items:
-        product = Product.objects.get(id=item['product']['id'])
+        product = Product.objects.get(id=item['id'])
         orderItem = OrderItem.objects.create(product=product, order=order, quantity=item['quantity'])
     return customer, order
